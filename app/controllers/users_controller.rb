c314b65d3030461318e33530e6540cb90ef5c403
Roman Blondel <rblondel23@gmail.com>
2019-11-06 01:23:24 +0100

    profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
+    @event_id_array = []
+    current_user.events.each do |event|
+      @event_id_array << event.id
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Event.destroy_all
 Attendance.destroy_all
 10.times do
   city = ["Wonderland", "LaCasaDeBays", "Ton quartier préféré de ton quartier préféré", "Bisounoursland"].sample
-  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10, supplemental: true), email: "#{Faker::Name.first_name}@yopmail.com",encrypted_password: 'password')
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10, supplemental: true), email: "#{Faker::Name.first_name}@yopmail.com",password: "password")
   event = Event.create(title: Faker::Game.title, description: Faker::Lorem.sentence(word_count: 120, supplemental: true),price: Faker::Number.between(from: 1, to: 1000), location: city, start_date: Faker::Date.forward(days: 23), duration: 10)
   attendance1 = Attendance.create(user: user, event: event)
 end
